823938d0e086ea33583868383773e2ab0c151cc6
Lance Finney <lance.finney@oasisdigital.com>
2023-11-10 11:12:35 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
       explore here. See the instructor's presentation to learn how to
       set up your own project.
     </p>
-  `
+  `,
+  standalone: true
 })
 export class AppComponent {}
